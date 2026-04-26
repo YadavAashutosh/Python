637e3b421dfdc80d1680a1dca381c118901e75e0
Ashu <ashuyadav210905@gmail.com>
2026-04-26 14:26:23 +0530

    Update program9.py

--- program9.py
+++ program9.py
@@ -15,7 +15,7 @@ that prints a general welcome message and school rules (without taking the self 
 and call the get_average() method for both students.
 '''
 
-print("===OOPS Concept===", end="\n\n")
+print("\n===OOPS Concept===", end="\n\n")
 
 class Student:
     school = "OOPS IN PYTHON"
